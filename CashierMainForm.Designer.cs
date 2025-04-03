2c8f4dedadca9213db798c696153dde8edbb1c80
Martin <chismartin96@gmail.com>
2025-04-03 15:35:56 -0700

    Add files via upload

---
+++ CashierMainForm.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace Inventory
+{
+    partial class CashierMainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // CashierMainForm
+            // 
+            this.ClientSize = new System.Drawing.Size(336, 261);
+            this.Name = "CashierMainForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button close;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button logOut_btn;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel3;
+        private AdminDashboard adminDashboard1;
+        private AdminAddProducts adminAddProducts1;
+        //private CashierOrder cashierOrder1;
+        //private CashierOrder cashierOrder2;
+    }
+}
